fc9f4e0a0a2f113c0c7e94ed1f2620930fc1ef7b
forsbergmaria <83494435+forsbergmaria@users.noreply.github.com>
2023-03-07 14:40:47 +0100

    Models

--- Models/Subcategory.cs
+++ Models/Subcategory.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-    internal class CategoryHasSubcategory
+    public class Subcategory
     {
+        [Key]
+        public int SubcategoryId { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Categories { get; set; }
     }
 }
