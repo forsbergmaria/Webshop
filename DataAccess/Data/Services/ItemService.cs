c3ab586292168c3f13501f3b832c6e8def48633b
mathildaberneby <83812840+mathildaberneby@users.noreply.github.com>
2023-03-22 23:04:53 +0100

    Skapat controller samt vyer för Items

---
+++ DataAccess/Data/Services/ItemService.cs
@@ -0,0 +1,64 @@
+﻿using Data;
+using Models;
+using Models.ViewModels;
+using System.Web.Mvc;
+
+namespace DataAccess.Data.Services
+{
+    public class ItemService
+    {
+        private ItemRepository itemRepository { get { return new ItemRepository(); } }
+        private CategoryRepository categoryRepository { get { return new CategoryRepository(); } }
+        public List<Item> SearchByCategory(string searchString)
+        {
+            List<Item> listofitems = itemRepository.GetAllItems();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                listofitems = listofitems.FindAll(c => c.Category.Name.ToLower()
+                .Contains(searchString.ToLower())).ToList();
+            }
+
+            return listofitems;
+        }
+
+        public List<SelectListItem> PopulateCategoryList()
+        {
+            var allCategories = categoryRepository.GetAllCategories();
+
+            List<SelectListItem> categories = allCategories.ConvertAll(c =>
+            {
+                return new SelectListItem()
+                {
+                    Text = c.Name.ToString(),
+                    Value = c.CategoryId.ToString(),
+                    Selected = false
+                };
+            });
+
+            return categories;
+        }
+
+        public ItemIndexView GetViewModel()
+        {
+            var items = itemRepository.GetAllItems();
+            var categories = categoryRepository.GetAllCategories();
+            var subs = categoryRepository.GetAllSubcategories();
+            ItemIndexView viewmodel = new ItemIndexView();
+
+            foreach(var item in items)
+            {
+                viewmodel.Items.Add(item);
+            }
+            foreach (var category in categories)
+            {
+                viewmodel.Categories.Add(category);
+            }
+            foreach (var sub in subs)
+            {
+                viewmodel.Subcategories.Add(sub);
+            }
+            return viewmodel;
+        }
+    }
+}
---
+++ Models/ViewModels/ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class ItemIndexView
+    {
+        public List<Item> Items { get; set; }
+        public List<Subcategory>? Subcategories { get; set; }
+        public List<Category>? Categories { get; set; }
+        public List<Size>? Sizes { get; set; }
+    }
+}
