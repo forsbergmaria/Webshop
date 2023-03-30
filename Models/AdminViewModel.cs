4b73bf25d0d267196abf4bf6176a8765d0f0bee9
forsbergmaria <83494435+forsbergmaria@users.noreply.github.com>
2023-03-30 15:51:37 +0200

    Småfix

--- Models/AdminViewModel.cs
+++ Models/AdminViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,13 @@ namespace Models
     public class AdminViewModel
     {
         public string Id { get; set; }
+        [Display(Name = "Förnamn")]
         public string? FirstName { get; set; }
+        [Display(Name = "Efternamn")]
         public string? LastName { get; set; }
+        [Display(Name = "E-postadress")]
         public string Email { get; set; }
+        [Display(Name = "Administratörsroll")]
         public string? Role { get; set; }
     }
 }
