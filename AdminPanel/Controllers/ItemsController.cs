31e4a60320d5a7bcac7d6c726935fa84a486ed61
forsbergmaria <83494435+forsbergmaria@users.noreply.github.com>
2023-04-12 12:46:56 +0200

    Fixat en väldigt temporär vy för att visa alla produkter. Ingen design ännu

--- AdminPanel/Controllers/ItemsController.cs
+++ AdminPanel/Controllers/ItemsController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 
 namespace AdminPanel.Controllers
 {
     public class ItemsController : Controller
     {
+        ItemRepository _itemRepository { get { return new ItemRepository(); } }
         [Authorize]
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult CreateItemForm()
+        {
+            return View("CreateItem");
+        }
+
+        public IActionResult CreateItem(Item item) 
+        {
+            _itemRepository.AddItem(item);
+            return RedirectToAction("AllItems");
+        }
+
+        public IActionResult AllItems() 
+        { 
+            List<Item> items = _itemRepository.GetAllItems();
+            return View(items);
         }
     }
 }
